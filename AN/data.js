443bce210d16dbd215c6e674884812c49447272f
Yanzi0818 <banyan818@gmail.com>
2024-11-01 12:03:25 +0800

    .

---
+++ AN/data.js
@@ -0,0 +1,260 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "另一頭",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": -0.4050405086593898,
+        "pitch": -0.06323135727196316,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.2184469841601775,
+          "pitch": 0.1690320235278726,
+          "rotation": 0.7853981633974483,
+          "target": "3-"
+        },
+        {
+          "yaw": 2.627419557588201,
+          "pitch": 0.1525230066235519,
+          "rotation": 12.566370614359176,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.08205743315574843,
+          "pitch": 0.33408267830726857,
+          "title": "小路",
+          "text": "向上的是爬和美山的步道<div>向下則通往世外桃源</div>"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "太平宮",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": -2.021651185347082,
+        "pitch": -0.01097066897161092,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.6664975738942402,
+          "pitch": 0.222841343495217,
+          "rotation": 0.7853981633974483,
+          "target": "0-"
+        },
+        {
+          "yaw": -1.5367215865460508,
+          "pitch": 0.23201260720608374,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.6454461181210682,
+          "pitch": -0.026135030976764284,
+          "title": "太平宮",
+          "text": "新店古老的廟宇之一，也是台灣百來座奉祀開漳聖王的廟宇中，頗具規模和代表性的一座"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "太平宮內部",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "yaw": 0.050444537902425424,
+        "pitch": 0.03593540863665545,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.123196398910739,
+          "pitch": 0.23260026894590347,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.9224537521774092,
+          "pitch": 0.043596281610746246,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "遠景圖",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1792,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8600924327377193,
+          "pitch": -0.053185389595697075,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": 0.24399165944947399,
+          "pitch": -0.8744812845625187,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.8047362615122875,
+          "pitch": 0.41616949204698983,
+          "title": "小路",
+          "text": "通往世外桃源的一條路"
+        },
+        {
+          "yaw": -0.374918929280172,
+          "pitch": 0.3686598313681593,
+          "title": "美麗的景色",
+          "text": "美麗的景色"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "空拍圖",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3439199950405865,
+          "pitch": 1.0163385275945842,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.648540733691485,
+          "pitch": 1.4382910961406736,
+          "title": "俯瞰新店",
+          "text": "美麗的空拍圖"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
