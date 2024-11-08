9914ed403bbc39d1ddb0c571cb1ac414d69431e1
Yanzi0818 <banyan818@gmail.com>
2024-11-08 11:21:55 +0800

    .

---
+++ Xindian/data.js
@@ -0,0 +1,447 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "吊橋下觀景台",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3328,
+      "initialViewParameters": {
+        "yaw": 1.5819417436082857,
+        "pitch": 0.11513895243215622,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.122991183446775,
+          "pitch": 0.19598430122048605,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": -0.11452508302400943,
+          "pitch": 0.2603063406861974,
+          "rotation": 0,
+          "target": "2-_"
+        },
+        {
+          "yaw": 1.077723149857725,
+          "pitch": -0.8330232520457859,
+          "rotation": 6.283185307179586,
+          "target": "5-"
+        },
+        {
+          "yaw": -1.6204517347879932,
+          "pitch": -0.053204204512715236,
+          "rotation": 0,
+          "target": "6-"
+        },
+        {
+          "yaw": 2.8349122147952013,
+          "pitch": 0.2756253538507014,
+          "rotation": 5.497787143782138,
+          "target": "7-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.2214879912665442,
+          "pitch": 0.4844615933812477,
+          "title": "觀景台",
+          "text": "可以在這裡看整個河面<br>在這附近吃從老街買來的東西或許不錯...?"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "花之步道",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3328,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.017292271470102705,
+          "pitch": 0.3921482741079423,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": 0.589488155677774,
+          "pitch": 0.2868580115010104,
+          "rotation": 0.7853981633974483,
+          "target": "7-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 3.0470211374516305,
+          "pitch": 0.16581699289122653,
+          "title": "花之步道",
+          "text": "整條路最美的風景之一<br>(斜坡上去就是新店捷運站了)"
+        }
+      ]
+    },
+    {
+      "id": "2-_",
+      "name": "美食走廊_前",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3328,
+      "initialViewParameters": {
+        "yaw": 1.780867324329785,
+        "pitch": 0.08009666256150005,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.14336691788276923,
+          "pitch": 0.21859640845219985,
+          "rotation": 0,
+          "target": "3-_"
+        },
+        {
+          "yaw": -3.029409460944567,
+          "pitch": 0.4864906058553622,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.05389505989097998,
+          "pitch": -0.013242295884687394,
+          "title": "美食",
+          "text": "有Pizza、碳烤、茶飲任君選擇"
+        }
+      ]
+    },
+    {
+      "id": "3-_",
+      "name": "美食走廊_中",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3328,
+      "initialViewParameters": {
+        "yaw": 1.420734441832714,
+        "pitch": 0.042799283049351544,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.136340167364052,
+          "pitch": 0.5540149975299293,
+          "rotation": 0,
+          "target": "2-_"
+        },
+        {
+          "yaw": -0.17785919857727706,
+          "pitch": 0.3100217336710429,
+          "rotation": 0,
+          "target": "4-_"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.4020446464967886,
+          "pitch": 0.7318777374198611,
+          "title": "舒適的沙發區",
+          "text": "坐在這邊看風景跟吃美食<br>是許多人難忘的回憶"
+        }
+      ]
+    },
+    {
+      "id": "4-_",
+      "name": "美食走廊_後",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3328,
+      "initialViewParameters": {
+        "yaw": 1.5369719691695005,
+        "pitch": 0.000026205206919982516,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0709706862425152,
+          "pitch": 0.6381406659458051,
+          "rotation": 0,
+          "target": "3-_"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.20003870985059002,
+          "pitch": 0.30618415653717257,
+          "title": "碧潭藝術大街",
+          "text": "雖然說是藝術大街，但好像是一條適合看景色的步道"
+        }
+      ]
+    },
+    {
+      "id": "5-",
+      "name": "吊橋中段",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3328,
+      "initialViewParameters": {
+        "yaw": 1.5439457236198368,
+        "pitch": 0.16400511356165381,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1001304088585186,
+          "pitch": 0.2308137046229497,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.2439216275195335,
+          "pitch": 0.45570497302159474,
+          "title": "吊橋中段",
+          "text": "走起來晃晃的，但上面的景色真的很美"
+        }
+      ]
+    },
+    {
+      "id": "6-",
+      "name": "入口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1415,
+      "initialViewParameters": {
+        "yaw": 2.923164592941043,
+        "pitch": -0.524934579509873,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.896866090671372,
+          "pitch": 0.0606943448116013,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.13000771027356883,
+          "pitch": 0.22320738471101542,
+          "title": "新店老街",
+          "text": "要買美食來這準沒錯!!"
+        }
+      ]
+    },
+    {
+      "id": "7-",
+      "name": "天鵝船碼頭",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3328,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1204565314225885,
+          "pitch": 0.3130604269925428,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": 3.090796673292883,
+          "pitch": 0.42044517083865607,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.753221948598263,
+          "pitch": 0.21869749372696745,
+          "title": "天鵝船",
+          "text": "跟旁邊的店家買票就可以乘坐<br>但要注意不要太累了喔"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
